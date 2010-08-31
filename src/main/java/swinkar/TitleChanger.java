fc26d7ea78e95dd34dca6143176707a84e3d216d
Peter Donald <peter@realityforge.org>
2010-08-31 22:01:24 +1000

    Add component that sends a message periodically to channel that title bar listens to

---
+++ src/main/java/swinkar/TitleChanger.java
@@ -0,0 +1,62 @@
+package swinkar;
+
+import java.util.Properties;
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.apache.felix.ipojo.handlers.event.publisher.Publisher;
+
+@Component( immediate = true )
+public class TitleChanger
+{
+  @org.apache.felix.ipojo.handlers.event.Publisher( name = "TitlePublisher", synchronous = false, topics = "MainFrame/Title", data_key = "title")
+  private Publisher m_publisher;
+  private Thread m_thread;
+  private boolean m_stop;
+
+  @Validate
+  public void start()
+  {
+    System.out.println( "TitleChanger.start" );
+    final Runnable runnable = new Runnable()
+    {
+      public void run()
+      {
+        m_stop = false;
+        while( !m_stop )
+        {
+          try
+          {
+            Thread.sleep( 1000 );
+          }
+          catch( InterruptedException e )
+          {
+          }
+          send();
+        }
+      }
+    };
+    m_thread = new Thread( runnable);
+    m_thread.start();
+  }
+
+  private void send()
+  {
+    final Properties data = new Properties();
+    data.setProperty("title", "Time: " + System.currentTimeMillis() );
+    m_publisher.send( data );
+  }
+
+  @Invalidate
+  public void stop()
+  {
+    m_stop = true;
+    try
+    {
+      m_thread.join( 1000 );
+    }
+    catch( InterruptedException e )
+    {       
+    }
+  }
+}
